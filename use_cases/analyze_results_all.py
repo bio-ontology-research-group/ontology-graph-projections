bb727844accdeec625880c2c518515172b26e819
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2023-03-09 00:08:59 +0300

    :construction:

--- use_cases/analyze_results_all.py
+++ use_cases/analyze_results_all.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import sys
+import click as ck
 
-
-def analyze_result_metric(file_path, metric, criterion="max"):
-    header = ["embed_dim", "margin", "reg", "batch_size", "lr", "mr", "mrr", "h1", "h10", "h100","auc"]
+def analyze_result_metric(file_path, metric, criterion="max", auc_threshold=0.7):
+    #header = ["embed_dim", "margin", "reg", "batch_size", "lr", "mr", "mrr", "h1", "h10", "h100","auc"]
+    header = ["embed_dim", "margin", "reg", "batch_size", "lr", "mr", "mrr", "h1", "h10", "h100","auc", "fmr", "fmrr", "fh1", "fh10", "fh100", "fauc"]
 
     df = pd.read_csv(filename, header=None, names=header)
-
+    df = df[df["auc"] > auc_threshold]
+    df = df[df['h1'] > 0]
                                                         
     if criterion == "max":
         best = df.loc[df[metric].idxmax()].to_frame().T
@@ -19,23 +21,66 @@ def analyze_result_metric(file_path, metric, criterion="max"):
     best["h10"] = best["h10"].apply(lambda x: round(x*100, 2))
     best["h100"] = best["h100"].apply(lambda x: round(x*100, 2))
     best["auc"] = best["auc"].apply(lambda x: round(x*100, 2))
-    best.round(4)
+    best["fmr"] = best["fmr"].apply(lambda x: round(x, 2))
+    best["fmrr"] = best["fmrr"].apply(lambda x: round(x, 2))
+    best["fh1"] = best["fh1"].apply(lambda x: round(x*100, 2))
+    best["fh10"] = best["fh10"].apply(lambda x: round(x*100, 2))
+    best["fh100"] = best["fh100"].apply(lambda x: round(x*100, 2))
+    best["fauc"] = best["fauc"].apply(lambda x: round(x*100, 2))
     return best
     
-            
-if __name__ == "__main__":
-    
-    filename = sys.argv[1]
 
-    best_mr = analyze_result_metric(filename, "mr", criterion="min")
-    best_mrr = analyze_result_metric(filename, "mrr")
-    best_h1 = analyze_result_metric(filename, "h1")
-    best_h10 = analyze_result_metric(filename, "h10")
-    best_h100 = analyze_result_metric(filename, "h100")
-    best_auc = analyze_result_metric(filename, "auc")
+
+def check_metrics_graph(filename, auc_threshold):    
+        
+    all_metrics = False
+
+    if all_metrics:
+        best_mr = analyze_result_metric(filename, "mr", criterion="min", auc_threshold=auc_threshold)
+        best_mrr = analyze_result_metric(filename, "mrr", criterion="max", auc_threshold=auc_threshold)
+        best_h1 = analyze_result_metric(filename, "h1", auc_threshold=auc_threshold)
+        best_h10 = analyze_result_metric(filename, "h10", auc_threshold=auc_threshold)
+        best_h100 = analyze_result_metric(filename, "h100", auc_threshold=auc_threshold)
+        best_auc = analyze_result_metric(filename, "auc", auc_threshold=auc_threshold)
+        best_fmr = analyze_result_metric(filename, "fmr", criterion="min", auc_threshold=auc_threshold)
+        best_fmrr = analyze_result_metric(filename, "fmrr", auc_threshold=auc_threshold)
+        best_fh1 = analyze_result_metric(filename, "fh1", auc_threshold=auc_threshold)
+        best_fh10 = analyze_result_metric(filename, "fh10", auc_threshold=auc_threshold)
+        best_fh100 = analyze_result_metric(filename, "fh100", auc_threshold=auc_threshold)
+        best_fauc = analyze_result_metric(filename, "fauc", auc_threshold=auc_threshold)
+        all_res = pd.concat([best_mr, best_mrr, best_h1, best_h10, best_h100, best_auc, best_fmr, best_fmrr, best_fh1, best_fh10, best_fh100, best_fauc], axis=0)
+        #swap_list = ["embed_dim", "margin", "reg", "batch_size", "lr", "mrr", "mr", "h1", "h10", "h100", "auc"]
+        swap_list = ["embed_dim", "margin", "reg", "batch_size", "lr", "mrr", "mr", "h1", "h10", "h100", "auc", "fmrr", "fmr", "fh1", "fh10", "fh100", "fauc"]
+        all_res = all_res.reindex(columns=swap_list)
+        print(all_res)
+    else:
+        best_h1 = analyze_result_metric(filename, "fh1", auc_threshold=auc_threshold)
+        #best_fh1 = analyze_result_metric(filename, "fh1")
+        all_res = best_h1 #pd.concat([best_h1, best_fh1], axis=0)
+        swap_list = ["embed_dim", "margin", "reg", "batch_size", "lr", "mrr", "mr", "h1", "h10", "h100", "auc", "fmrr", "fmr", "fh1", "fh10", "fh100", "fauc"]
+        all_res = all_res.reindex(columns=swap_list)
+        print(all_res)
+        all_res = list(all_res.iloc[0])
+        tex_str = " & ".join([str(x) for x in all_res])
+        print(tex_str)
+        
+
+
+
+if __name__ == "__main__":
+
+    import os
     
-    all_res = pd.concat([best_mr, best_mrr, best_h1, best_h10, best_h100, best_auc])
-    swap_list = ["embed_dim", "margin", "reg", "batch_size", "lr", "mrr", "mr", "h1", "h10", "h100", "auc"]
-    all_res = all_res.reindex(columns=swap_list)
-    print(all_res)
+    directory = sys.argv[1]
+    case = sys.argv[2]
+    test = sys.argv[3]
+    reduced = sys.argv[4]
+
+    suffix = ".csv"
+    if reduced == "True":
+        suffix = "_red.csv"
+
+    if test == "sub":
+        
+    
 
---
+++ use_cases/cat_results.py
@@ -0,0 +1,54 @@
+
+embed_dims = ["64", "128", "256"]
+margins = ["0.0", "0.2", "0.4"]
+
+def cat_results(directory, case, graph, test, reduced, both_quants):
+
+    if case not in ["go", "foodon"]:
+        raise ValueError("Case must be either 'go' or 'foodon'")
+    if graph not in ["taxonomy", "owl2vec", "onto2graph", "rdf"]:
+        raise ValueError("Graph must be either 'taxonomy', 'owl2vec', 'onto2graph' or 'rdf'")
+    if test not in ["sub", "ex"]:
+        raise ValueError("Test must be either 'sub' or 'ex'")
+    if reduced not in ["True", "False"]:
+        raise ValueError("Reduced must be either 'True' or 'False'")
+    if both_quants not in ["True", "False"]:
+        raise ValueError("Both quants must be either 'True' or 'False'")
+
+    
+    all_results = []
+    for embed_dim in embed_dims:
+        for margin in margins:
+            filename = directory + "/" + case + "_" + graph + "_" + embed_dim + "_" + margin + "_" + test + ".csv"
+            if reduced == "True":
+                filename = filename.replace(".csv", "_red.csv")
+            if both_quants == "True":
+                filename = filename.replace(".csv", "_both_quants.csv")
+                
+            with open(filename, "r") as f:
+                for line in f.readlines():
+                    all_results.append(line)
+    final_file = directory + "/" + case + "_" + graph + "_" + test + ".csv"
+    if reduced == "True":
+        final_file = final_file.replace(".csv", "_red.csv")
+    if both_quants == "True":
+        final_file = final_file.replace(".csv", "_both_quants.csv")
+           
+    with open(final_file, "w") as f:
+        for res in all_results:
+            f.write(f"{res}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    directory = sys.argv[1]
+    case = sys.argv[2]
+    graph = sys.argv[3]
+    test = sys.argv[4]
+    reduced = sys.argv[5]
+    both_quants = sys.argv[6]
+    cat_results(directory, case, graph, test, reduced, both_quants)
+    
+
+
