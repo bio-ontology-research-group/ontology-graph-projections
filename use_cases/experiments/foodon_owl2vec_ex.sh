4f3cabcfaed9db83982a05aace5ace2cc4ebae76
Fernando Zhapa Camacho <fernando.zhapacamacho@kaust.edu.sa>
2023-03-17 00:09:31 +0300

    added experiments commands for foodon

--- use_cases/experiments/foodon_owl2vec_ex.sh
+++ use_cases/experiments/foodon_owl2vec_ex.sh
@@ -1,2 +1,3 @@
 python ../run_model.py -case foodon -kge transe -g owl2vec -r ../foodon/data -dim 256 -m 0.4 -wd 0.0001 -bs 16384 -lr 0.001 -tbs 8 -e 4000 -d cuda -rd result_foodon_owl2vec_ex_transe.csv -tf ../foodon/data/foodon_existential_subsumption_closure_filtered_non_trivial_no_leakage.csv -te -tbq
 
+
